0845741bf5a01f6c32fef46fd31986d12f7f77a1
Doronov-IV <ipv2743@gmail.com>
2022-09-17 22:40:13 +0300

    Start secondary form design (#3)

--- c#/homework/windows-forms/3. Streamlet/#Forms/SecondaryForm.Designer.cs
+++ c#/homework/windows-forms/3. Streamlet/#Forms/SecondaryForm.Designer.cs
@@ -28,20 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LowerStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.MainRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.TextMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStrip = new System.Windows.Forms.ToolStrip();
+            this.TextMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // LowerStatusStrip
+            // 
+            this.LowerStatusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.LowerStatusStrip.Location = new System.Drawing.Point(0, 531);
+            this.LowerStatusStrip.Name = "LowerStatusStrip";
+            this.LowerStatusStrip.Size = new System.Drawing.Size(1182, 22);
+            this.LowerStatusStrip.TabIndex = 0;
+            this.LowerStatusStrip.Text = "statusStrip1";
+            // 
+            // MainRichTextBox
+            // 
+            this.MainRichTextBox.Location = new System.Drawing.Point(12, 56);
+            this.MainRichTextBox.Name = "MainRichTextBox";
+            this.MainRichTextBox.Size = new System.Drawing.Size(1158, 472);
+            this.MainRichTextBox.TabIndex = 1;
+            this.MainRichTextBox.Text = "";
+            // 
+            // TextMenuStrip
+            // 
+            this.TextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.TextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.видToolStripMenuItem,
+            this.настройкиToolStripMenuItem});
+            this.TextMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.TextMenuStrip.Name = "TextMenuStrip";
+            this.TextMenuStrip.Size = new System.Drawing.Size(1182, 28);
+            this.TextMenuStrip.TabIndex = 2;
+            this.TextMenuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.файлToolStripMenuItem.Text = "File";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.видToolStripMenuItem.Text = "View";
+            this.видToolStripMenuItem.Click += new System.EventHandler(this.видToolStripMenuItem_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.настройкиToolStripMenuItem.Text = "Settings";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.открытьToolStripMenuItem.Text = "Open";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьToolStripMenuItem.Text = "Save";
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьКакToolStripMenuItem.Text = "Save as";
+            // 
+            // ToolStrip
+            // 
+            this.ToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ToolStrip.Location = new System.Drawing.Point(0, 28);
+            this.ToolStrip.Name = "ToolStrip";
+            this.ToolStrip.Size = new System.Drawing.Size(1182, 25);
+            this.ToolStrip.TabIndex = 3;
+            this.ToolStrip.Text = "toolStrip1";
             // 
             // SecondaryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.ToolStrip);
+            this.Controls.Add(this.MainRichTextBox);
+            this.Controls.Add(this.LowerStatusStrip);
+            this.Controls.Add(this.TextMenuStrip);
+            this.MainMenuStrip = this.TextMenuStrip;
             this.Name = "SecondaryForm";
             this.Text = "Edit File Text";
-            this.Load += new System.EventHandler(this.SecondaryForm_Load);
+            this.Load += new System.EventHandler(this.OnSecondaryFormLoad);
+            this.TextMenuStrip.ResumeLayout(false);
+            this.TextMenuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private StatusStrip LowerStatusStrip;
+        private RichTextBox MainRichTextBox;
+        private MenuStrip TextMenuStrip;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem открытьToolStripMenuItem;
+        private ToolStripMenuItem сохранитьToolStripMenuItem;
+        private ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private ToolStripMenuItem видToolStripMenuItem;
+        private ToolStripMenuItem настройкиToolStripMenuItem;
+        private ToolStrip ToolStrip;
     }
 }
--- c#/homework/windows-forms/3. Streamlet/#Forms/SecondaryForm.cs
+++ c#/homework/windows-forms/3. Streamlet/#Forms/SecondaryForm.cs
@@ -19,12 +19,60 @@ namespace Streamlet.Forms
     /// </summary>
     public partial class SecondaryForm : Form
     {
+
+
+
+
+        #region PROPERTIES
+
+
+        /// <summary>
+        /// A reference to a current file;
+        /// <br />
+        /// Ссылка на текущий файл;
+        /// </summary>
+        private FileInfo _CurrentFileInfo;
+
+        /// <summary>
+        /// @see 'private FileInfo _CurrentFileInfo';
+        /// </summary>
+        public FileInfo CurrentFileInfo { get { return _CurrentFileInfo; } set { _CurrentFileInfo = value; } }
+
+
+        #endregion PROPERTIES
+
+
+        #region CONSTRUCTION
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
         public SecondaryForm()
         {
             InitializeComponent();
         }
 
-        private void SecondaryForm_Load(object sender, EventArgs e)
+
+        /// <summary>
+        /// Notebook form laod handler;
+        /// <br />
+        /// Хендлер загрузки формы текстового редактора;
+        /// </summary>
+        private void OnSecondaryFormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        #endregion CONSTRUCTION
+
+        private void видToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
