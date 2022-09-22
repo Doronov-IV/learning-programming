d10a3a7883a5d94817d982acb56fae937d6d494b
Doronov-IV <ipv2743@gmail.com>
2022-09-22 14:31:53 +0300

    Create and set up an ado project (#6)

---
+++ c#/homework/wpf/wpf-homeworks/AdoNetHomework/.view/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Configuration;
+global using System.Data;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.Windows;
+
+namespace AdoNetHomework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
