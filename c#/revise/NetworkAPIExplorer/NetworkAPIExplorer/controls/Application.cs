a0d6f07488108c14e26dce37cb6f52f9c878d9eb
Doronov-IV <ipv2743@gmail.com>
2022-10-06 16:05:11 +0300

    Add main networking project

---
+++ c#/revise/NetworkAPIExplorer/NetworkAPIExplorer/controls/Application.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkAPIExplorer.Controls
+{
+    public class Application
+    {
+
+
+        #region API - public Contract
+
+
+        /// <summary>
+        /// Run application;
+        /// <br />
+        /// Запустить приложение;
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+
+        #endregion API - public Contract
+
+
+
+
+
+
+        #region LOGIC - internal behavior
+
+
+
+
+
+        #endregion LOGIC - internal behavior
+
+
+
+
+
+
+
+        #region CONSTRUCTION
+
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
+        public Application()
+        {
+        }
+
+
+
+        #endregion CONSTRUCTION
+
+
+    }
+}
