37a9f16b3c084d550ad00956ad09c2ecf3915a5d
Doronov-IV <ipv2743@gmail.com>
2022-10-10 16:30:36 +0300

    Prepare to split this project in two (#9)

--- c#/revise/NetworkingSolution/MainNetworkingProject/.to-dll/IPendingOutput.cs
+++ c#/revise/NetworkingSolution/MainNetworkingProject/.to-dll/IPendingOutput.cs
@@ -1,18 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MainNetworkingProject.Misc
 {
+    /// <summary>
+    /// A simple set of tools to provide objects with ability to be set up in different I/O conditions;
+    /// <br />
+    /// Простой набор инструментов, чтобы предоставить объектам возмонжость быть использоваными в разных системах ввода/вывода;
+    /// </summary>
     public interface IPendingOutput
     {
 
         public delegate void OutputDelegate(string sOutputMessage);
 
+
         public event OutputDelegate SendOutput;
 
+
+        /// <summary>
+        /// Delay text message output till you'll be able to pass it out somewhere;
+        /// <br />
+        /// How to: In a constructor of a wrapper-class (or ui-class) you should '+=' it to the handler method;
+        /// <br />
+        /// <br />
+        /// Отложить вывод сообщения до тех пор, пока вы не сможете его где-нибудь вывести;
+        /// <br />
+        /// Как работает: В конструкторе класса-обёртки (или класса-визуального-интерфейса) передать его методу-обработчику через "+="; 
+        /// </summary>
         public void PendOutput(string Message);
 
     }
