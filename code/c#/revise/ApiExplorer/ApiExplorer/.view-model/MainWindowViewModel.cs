14325c07b0c142a4175c715622bdf574add1510b
Doronov-IV <ipv2743@gmail.com>
2022-10-14 19:40:17 +0300

    Im having a boring class

--- code/c#/revise/ApiExplorer/ApiExplorer/.view-model/MainWindowViewModel.cs
+++ code/c#/revise/ApiExplorer/ApiExplorer/.view-model/MainWindowViewModel.cs
@@ -2,13 +2,28 @@
 
 namespace ApiExplorer.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+
 
         #region PROPERTIES
 
 
-        //
+
+        private HttpClient _Client;
+
+        private ObservableCollection<string> _ListOfProperties;
+
+        public ObservableCollection<string> ListOfProperties
+        {
+            get { return _ListOfProperties; }
+            set
+            {
+                _ListOfProperties = value;
+                OnPropertyChanged(nameof(ListOfProperties));
+            }
+        }
+
 
 
         #endregion PROPERTIES
@@ -16,10 +31,90 @@ namespace ApiExplorer.ViewModel
 
 
 
+        #region COMMANDS
+
+
+        public DelegateCommand LoadCommand { get; }
+
+
+        #endregion COMMANDS
+
+
+
+
+        #region API
+
+
+        private void FillPropertiesList()
+        {
+            MethodInfo[] info = typeof(HttpClient).GetMethods();
+
+            StringBuilder builder = new();
+
+            foreach(var el in info)
+            {
+                builder = new();
+                builder.Append("(");
+                foreach(var param in el.GetParameters())
+                {
+                    builder.Append(param.Name + ", ");
+                }
+                if (builder.Length > 2) builder[builder.Length - 2] = ')';
+                ListOfProperties.Add(el.Name + builder.ToString().Trim());
+            }
+        }
+
+
+        #endregion API
+
+
+
+
         #region CONSTRUCTION
 
 
-        
+
+
+        #region Property changed
+
+
+        /// <summary>
+        /// Propery changed event handler;
+        /// <br />
+        /// Делегат-обработчик события 'property changed';
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        /// <summary>
+        /// Handler-method of the 'property changed' delegate;
+        /// <br />
+        /// Метод-обработчик делегата 'property changed';
+        /// </summary>
+        /// <param name="propName">The name of the property;<br />Имя свойства;</param>
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+        #endregion Property changed
+
+
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            _Client = new();
+            _ListOfProperties = new();
+
+            LoadCommand = new(FillPropertiesList);
+        }
 
 
         #endregion CONSTRUCTION
