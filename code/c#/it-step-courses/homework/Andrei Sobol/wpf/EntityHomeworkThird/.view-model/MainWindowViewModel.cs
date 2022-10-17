1249bc919d75f3565cea33616531b735e737e69b
Doronov-IV <ipv2743@gmail.com>
2022-10-17 16:05:04 +0300

    Split third homework proj (#6)

---
+++ code/c#/it-step-courses/homework/Andrei Sobol/wpf/EntityHomeworkThird/.view-model/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace EntityHomeworkThird.ViewModel
+{
+    public class MainWindowViewModel
+    {
+
+
+        #region PROPERTIES
+
+
+        /// <summary>
+        /// Handler is a file with all event handler methods;
+        /// <br />
+        /// Handler - это файл со всеми методами обработки событий;
+        /// </summary>
+        private MainWindowViewModelHandler _Handler;
+
+
+        #endregion PROPERTIES
+
+
+
+
+
+        #region API
+
+
+        //
+
+
+        #endregion API
+
+
+
+
+
+        #region CONSTRUCTION
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            _Handler = new(this);
+        }
+
+
+        #endregion CONSTRUCTION
+
+    }
+}
---
+++ code/c#/it-step-courses/homework/Andrei Sobol/wpf/EntityHomeworkThird/.view-model/MainWindowViewModelHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace EntityHomeworkThird.ViewModel
+{
+    public class MainWindowViewModelHandler
+    {
+
+
+        #region PROPERTIES
+
+
+
+        /// <summary>
+        /// ViewModel reference;
+        /// <br />
+        /// Ссылка на вью-модель;
+        /// </summary>
+        private MainWindowViewModel _ViewModelReference;
+
+
+
+        #endregion PROPERTIES
+
+
+
+
+
+        #region CONSTRUCTION
+
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
+        public MainWindowViewModelHandler(MainWindowViewModel ViewModelReference)
+        {
+            _ViewModelReference = ViewModelReference;
+        }
+
+
+
+        #endregion CONSTRUCTION
+
+
+    }
+}
