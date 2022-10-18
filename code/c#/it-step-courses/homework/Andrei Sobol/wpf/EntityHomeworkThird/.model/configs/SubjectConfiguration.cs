7c4a87c4746a78da40635bca0715201ccd28f411
Doronov-IV <ipv2743@gmail.com>
2022-10-18 10:36:46 +0300

    Add new entities and configs (#6)

---
+++ code/c#/it-step-courses/homework/Andrei Sobol/wpf/EntityHomeworkThird/.model/configs/SubjectConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EntityHomeworkThird.Model.Entities;
+
+namespace EntityHomeworkThird.Model.Configs
+{
+    public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+    {
+
+
+        /*
+        Добавить таблицу Subject[s](Предмет). Связать ее с таблицей Cards 
+    отношением многие ко многим - карта может иметь много предметов, 
+    каждый предмет может быть привязан к множеству карт
+        */
+
+
+        public void Configure(EntityTypeBuilder<Subject> subjectBuilder)
+        {
+            subjectBuilder.HasKey(sub => sub.Id);
+            subjectBuilder.Property(sub => sub.Name).HasMaxLength(50);
+
+            subjectBuilder.HasMany(card => card.CardList).WithMany(subj => subj.SubjectList);
+            
+        }
+    }
+}
--- code/c#/it-step-courses/homework/Andrei Sobol/wpf/EntityHomeworkThird/.model/entities/Card.cs
+++ code/c#/it-step-courses/homework/Andrei Sobol/wpf/EntityHomeworkThird/.model/entities/Card.cs
@@ -16,5 +16,22 @@
         public Student Student { get; set; } = null!;
 
 
+        public List<Subject>? SubjectList { get; set; }
+
+
+        public List<Mark>? MarkList { get; set; }
+
+
+
+        /// <summary>
+        /// Default constructor;
+        /// <br />
+        /// Конструктор по умолчанию;
+        /// </summary>
+        public Card()
+        {
+            SubjectList = new();
+            MarkList = new();
+        }
     }
 }
