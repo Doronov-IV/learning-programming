a2c06e0cfebb29baa107965cac316f413428739c
DoronovIV <ipv2743@gmail.com>
2022-11-08 14:49:31 +0300

    Add concurrency project (#12)

---
+++ code/c#/other-courses/ConcurrencySolution/MainConcurrencyProject/.view-model/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿namespace MainConcurrencyProject.view_model
+{
+    public partial class MainWindowViewModel
+    {
+
+
+        #region STATE
+
+
+        //
+
+
+        #endregion STATE
+
+
+
+
+
+
+
+        #region COMMANDS
+
+
+        //
+
+
+        #endregion COMMANDS
+
+
+
+
+
+
+
+        #region CONSTRUCTION
+
+
+
+        /// <summary>
+        /// Default constructor.
+        /// <br />
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+
+        }
+
+
+
+        #region property changed
+
+
+
+
+
+        #endregion property changed
+
+
+        #endregion CONSTRUCTION
+
+
+    }
+}
