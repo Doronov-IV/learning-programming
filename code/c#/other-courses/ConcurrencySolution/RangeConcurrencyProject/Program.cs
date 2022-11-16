82f641d3bd6a56cc21f0846c21038eca8f004c62
DoronovIV <ipv2743@gmail.com>
2022-11-16 16:16:00 +0300

    Add range project (#12)

---
+++ code/c#/other-courses/ConcurrencySolution/RangeConcurrencyProject/Program.cs
@@ -0,0 +1,29 @@
+﻿global using System;
+using System.Diagnostics;
+
+namespace Range
+{
+    public class Program
+    {
+
+        public static Stopwatch sw = new();
+
+        public static List<Task> tasks = new();
+
+        public static async Task Main(string[] args)
+        {
+            Task<int> primeBumberTask = Task.Run(() =>
+            Enumerable.Range(2, 20000000).Count(n =>
+            Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+
+            Console.WriteLine("Task running.....");
+            Console.WriteLine("The answer is " + primeBumberTask.Result);
+        }
+
+
+        private static void WaitThreeSecAsync()
+        {
+            Task.Delay(3000).Wait();
+        }
+    }
+}
