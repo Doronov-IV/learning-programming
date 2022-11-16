fc8ac26ecca70ce21b0a7d90d51b8c258c7b6b11
DoronovIV <ipv2743@gmail.com>
2022-11-16 17:50:48 +0300

    Add prime numbers count (#12)

--- code/c#/other-courses/ConcurrencySolution/RangeConcurrencyProject/Program.cs
+++ code/c#/other-courses/ConcurrencySolution/RangeConcurrencyProject/Program.cs
@@ -12,13 +12,28 @@ namespace Range
 
         public static async Task Main(string[] args)
         {
-            await Task.Run(() => WaitThreeSecAsync()).ContinueWith((a) => Console.WriteLine("Done!"));
+            await Task.Run(() => DisplayPrimesCount());
         }
 
 
         private static void WaitThreeSecAsync()
         {
             Task.Delay(3000).Wait();
+        }
+
+
+        public static async Task DisplayPrimesCount()
+        {
+            int result = await GetPrimesCountAsync(2, 1000000);
+            Console.WriteLine (result);
+        }
+
+
+        public static Task<int> GetPrimesCountAsync (int start, int count)
+        {
+            return Task.Run(() =>
+                ParallelEnumerable.Range(start, count).Count(n =>
+                Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
         }
     }
 }
