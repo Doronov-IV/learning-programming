6bd247c9997b23c9098c248bbdf231dc7cc03e3f
DoronovIV <ipv2743@gmail.com>
2022-11-22 15:31:36 +0300

    Add task two from metanit (#12)

---
+++ code/c#/misc/ShootingRangeSolution/ShootingRangeProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ShootingRange
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var s = new S();
+            using (s)
+            {
+                Console.WriteLine(s.GetDispose());
+            }
+            Console.WriteLine(s.GetDispose());
+        }
+    }
+
+
+
+    public struct S : IDisposable
+    {
+        public bool dispose;
+        public void Dispose()
+        {
+            dispose = true;
+        }
+        public bool GetDispose()
+        {
+            return dispose;
+        }
+    }
+}
