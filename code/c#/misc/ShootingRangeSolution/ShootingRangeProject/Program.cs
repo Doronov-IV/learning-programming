0100c70a91ddef846012b5a9390748785ef57fc2
DoronovIV <ipv2743@gmail.com>
2022-11-22 17:09:03 +0300

    Begin part 3 from metanit (#9)

--- code/c#/misc/ShootingRangeSolution/ShootingRangeProject/Program.cs
+++ code/c#/misc/ShootingRangeSolution/ShootingRangeProject/Program.cs
@@ -4,14 +4,45 @@ namespace ShootingRange
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        static IEnumerable<int> Square(IEnumerable<int> a)
         {
-            int i = 1;
-            object obj = i;
-            ++i;
-            Console.WriteLine(i);
-            Console.WriteLine(obj);
-            Console.WriteLine(obj);
+            foreach (var r in a)
+            {
+                Console.WriteLine(r * r);
+                yield return r * r;
+            }
+        }
+        class Wrap
+        {
+            private static int init = 0;
+            public int Value
+            {
+                get { return ++init; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var w = new Wrap();
+            var wraps = new Wrap[3];
+            for (int i = 0; i < wraps.Length; i++)
+            {
+                wraps[i] = w;
+            }
+
+            var values = wraps.Select(x => x.Value);
+            var results = Square(values);
+            int sum = 0;
+            int count = 0;
+            foreach (var r in results)
+            {
+                count++;
+                sum += r;
+            }
+            Console.WriteLine("Count {0}", count);
+            Console.WriteLine("Sum {0}", sum);
+
+            Console.WriteLine("Count {0}", results.Count());
+            Console.WriteLine("Sum {0}", results.Sum());
         }
     }
 }
