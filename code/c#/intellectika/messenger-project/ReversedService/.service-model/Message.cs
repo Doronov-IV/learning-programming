79e66c62e10d213e4525a7f30b099e421946ceac
DoronovIV <ipv2743@gmail.com>
2022-11-29 11:03:21 +0300

    Add message entity (#12)

---
+++ code/c#/intellectika/messenger-project/ReversedService/.service-model/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedService.Model
+{
+    /// <summary>
+    /// An entity of a 'Messages' table.
+    /// <br />
+    /// Представитель таблицы "Messages".
+    /// </summary>
+    public class Message
+    {
+
+        public int SenderId { get; set; }
+
+
+        public int RecieverId { get; set; }
+
+
+        public string MessageContents { get; set; } = null!;
+
+
+
+        /// <summary>
+        /// Default constructor.
+        /// <br />
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Message()
+        {
+
+        }
+
+    }
+}
