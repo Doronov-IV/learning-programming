b99d724424023e922b3354a6bd32e2e0aae083e0
DoronovIV <ipv2743@gmail.com>
2022-12-20 12:36:37 +0300

    Fix client spare connection exception (#12)

--- code/c#/intellectika/messenger-project/ReversedClient/.client-view-model/ClientLoginWindow/ClientLoginWindowViewModelHandler.cs
+++ code/c#/intellectika/messenger-project/ReversedClient/.client-view-model/ClientLoginWindow/ClientLoginWindowViewModelHandler.cs
@@ -16,30 +16,65 @@ namespace ReversedClient.ViewModel.ClientStartupWindow
         /// </summary>
         public async void OnSignInButtonClick()
         {
+            // first and main try;
             try
             {
-                if (!string.IsNullOrEmpty(_localUserTechnicalData.Password) && !string.IsNullOrEmpty(_localUserTechnicalData.Login))
-                {
-                    if (ServiceTransmitter.ConnectAndAuthorize(_localUserTechnicalData))
-                    {
-                        ServiceTransmitter.ConnectAndSendLoginToService(_localUserTechnicalData);
-                        FullUserServiceData = ServiceTransmitter.GetResponseData(); // deadlock
+                MakeConnectionEffort();
+            }
 
-                        WpfWindowsManager.MoveFromLoginToChat(FullUserServiceData, ServiceTransmitter);
-                    }
-                    else
+            catch (Exception ex)
+            {
+                // second try;
+                Task.Delay(20).Wait();
+                try
+                {
+                    MakeConnectionEffort();
+                }
+
+                catch
+                {
+                    // third try;
+                    Task.Delay(20).Wait();
+                    try
                     {
-                        MessageBox.Show("Authorization failed due to the incurrect data input.", "Please, check your input", MessageBoxButton.OK, MessageBoxImage.Hand);
+                        MakeConnectionEffort();
                     }
+
+                    catch
+                    {
+                        // the server(s) is(are) down after all;
+                        MessageBox.Show($"Service is down. Consider connecting later.", "Unable to connect", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Make an effort to connect to the authorization service.
+        /// <br />
+        /// Сделать попытку подклюяиться к сервису авторизации.
+        /// </summary>
+        private void MakeConnectionEffort()
+        {
+            if (!string.IsNullOrEmpty(_localUserTechnicalData.Password) && !string.IsNullOrEmpty(_localUserTechnicalData.Login))
+            {
+                if (ServiceTransmitter.ConnectAndAuthorize(_localUserTechnicalData))
+                {
+                    ServiceTransmitter.ConnectAndSendLoginToService(_localUserTechnicalData);
+                    FullUserServiceData = ServiceTransmitter.GetResponseData(); // deadlock
+
+                    WpfWindowsManager.MoveFromLoginToChat(FullUserServiceData, ServiceTransmitter);
                 }
                 else
                 {
-                    MessageBox.Show("Both login and password fields are required to proceed.", "Please, check your input", MessageBoxButton.OK, MessageBoxImage.Hand);
+                    MessageBox.Show("Authorization failed due to the incurrect data input.", "Please, check your input", MessageBoxButton.OK, MessageBoxImage.Hand);
                 }
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show($"Service is down. Consider connecting later.", "Unable to connect", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Both login and password fields are required to proceed.", "Please, check your input", MessageBoxButton.OK, MessageBoxImage.Hand);
             }
         }
 
