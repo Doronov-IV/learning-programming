30d2e01f5ed54805ea015ac5f0ee1f307a132c54
Doronov-IV <ipv2743@gmail.com>
2022-12-24 23:14:00 +0300

    Add message interface

---
+++ code/c#/intellectika/messenger-project/NetworkingAuxiliaryLibrary/.net/auxiliary/packs/IMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkingAuxiliaryLibrary.Packages
+{
+    public interface IMessage
+    {
+
+        public string GetSender();
+
+        public string GetReceiver();
+
+        public string GetDate();
+
+        public string GetTime();
+
+        public object GetMessage();
+
+
+    }
+}
