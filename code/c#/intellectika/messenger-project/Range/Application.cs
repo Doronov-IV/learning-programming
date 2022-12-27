3c208fe769a8216d7057aee1fa25da64ed948ce2
DoronovIV <ipv2743@gmail.com>
2022-12-27 17:59:33 +0300

    Clear the project (#19)

--- code/c#/intellectika/messenger-project/Range/Application.cs
+++ code/c#/intellectika/messenger-project/Range/Application.cs
@@ -1,50 +1,17 @@
-﻿namespace Range
+﻿global using System;
+global using Newtonsoft.Json;
+global using NetworkingAuxiliaryLibrary.Processing;
+global using NetworkingAuxiliaryLibrary.Packages;
+global using System.Text;
+global using System.Reflection;
+
+namespace Range
 {
     public class Application
     {
-
-        public async Task Run()
+        public void Run()
         {
-            Console.WriteLine("Run start.");
-
-            Task task = new(Foo);
-
-            task.Start();
             
-            var awaiter = task.GetAwaiter();
-
-            task.ContinueWith((t) =>
-            {
-                var t2 = new Task(Done);
-
-                t2.RunSynchronously();
-
-                return t2;
-
-            }, TaskContinuationOptions.AttachedToParent);
-
-            Console.WriteLine("Run end.");
-
-            Console.ReadKey();
-        }
-
-        public void Foo()
-        {
-            Console.WriteLine($"Foo start. Thread {Thread.CurrentThread.ManagedThreadId}");
-
-            Thread.Sleep(5000);
-
-            Console.WriteLine("Foo end.");
-        }
-
-
-        public void Done()
-        {
-            Console.WriteLine($"Done start. Thread {Thread.CurrentThread.ManagedThreadId}");
-
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Done end.");
         }
     }
 }
--- code/c#/intellectika/messenger-project/Range/Program.cs
+++ code/c#/intellectika/messenger-project/Range/Program.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using Newtonsoft.Json;
-using NetworkingAuxiliaryLibrary.Processing;
-using NetworkingAuxiliaryLibrary.Packages;
-using System.Text;
-
-namespace Range // Note: actual namespace depends on the project name.
+﻿namespace Range
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine($"Main start. Thread {Thread.CurrentThread.ManagedThreadId}");
             Application app = new();
             app.Run();
-            Console.WriteLine($"Main end.");
-            Console.ReadKey();
         }
     }
 }
