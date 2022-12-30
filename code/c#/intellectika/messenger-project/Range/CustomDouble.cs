39dcab6bd00f5bc8a050fbf44871b35dbb8e3563
DoronovIV <ipv2743@gmail.com>
2022-12-30 10:49:21 +0300

    Create an array and two data types (#22)

---
+++ code/c#/intellectika/messenger-project/Range/CustomDouble.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    public class CustomDouble : IDataType
+    {
+        private double _value;
+
+        public double Value 
+        { 
+            get { return _value;} 
+            set { _value = value; } 
+        }
+
+        public object GetValue()
+        {
+            return Value;
+        }
+
+
+        /// <summary>
+        /// Default constructor.
+        /// <br />
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public CustomDouble()
+        {
+            _value = 0.0d;
+        }
+
+
+        public CustomDouble(double value)
+        {
+            _value = value;
+        }
+    }
+}
---
+++ code/c#/intellectika/messenger-project/Range/DynamicTypedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    public class DynamicTypedList : IEnumerable<IDataType>
+    {
+
+        #region STATE
+
+
+        private IDataType[] array;  
+
+
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        public IDataType? Last
+        {
+            get
+            {
+                if (Count >= 0) return array[Count - 1];
+                else return null;
+            }
+            set
+            {
+                if (Count >= 0) array[Count - 1] = value;
+                else throw new NullReferenceException("[Custom] The list was empty.");
+            }
+        }
+
+        public IDataType? First
+        {
+            get 
+            {
+                if (Count >= 0) return array[0];
+                else return null;
+            }
+            set
+            {
+                if (Count >= 0) array[0] = value;
+                else throw new NullReferenceException("[Custom] The list was empty."); 
+            }
+        }
+
+
+        #endregion STATE
+
+
+        public IEnumerator<IDataType> GetEnumerator()
+        {
+            foreach (IDataType item in array)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
+        /// <summary>
+        /// Default constructor.
+        /// <br />
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public DynamicTypedList()
+        {
+            array = new IDataType[0];
+        }
+
+
+        public void Add(IDataType item)
+        {
+            int nNewSize = Count + 1;
+            int nOldSize = Count;
+
+            array = new IDataType[nNewSize];
+
+            array[nOldSize] = item;
+        }
+    }
+}
