8393b72ac95a37e8e04bc072c0b6db95318dde61
DoronovIV <ipv2743@gmail.com>
2023-02-06 17:32:07 +0300

    Last entry commit

---
+++ code/c#/intellectika/efc/TankFactoryProject/Generation/TankFactories/Generator/MainBattleTankRandomGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainEntityProject.Generation.TankFactories
+{
+    public static class MainBattleTankRandomGenerator
+    {
+
+        public static MainBattleTank GetNext()
+        {
+            MainBattleTank mbt = new();
+
+            return mbt;
+        }
+
+    }
+}
