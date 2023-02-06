cdbfb47ee1144a7955d8baedfa924177caf51d12
DoronovIV <ipv2743@gmail.com>
2023-02-06 11:22:41 +0300

    Cut out all images from the student doc

--- code/c#/misc/ShootingRangeSolution/ShootingRangeProject/Program.cs
+++ code/c#/misc/ShootingRangeSolution/ShootingRangeProject/Program.cs
@@ -16,26 +16,45 @@ namespace ShootingRange
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        static IEnumerable<int> Square(IEnumerable<int> a)
         {
+            foreach (var r in a)
+            {
+                Console.WriteLine(r * r);
+                yield return r * r;
+            }
+        }
+        class Wrap
+        {
+            private static int init = 0;
+            public int Value
+            {
+                get { return ++init; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var w = new Wrap();
+            var wraps = new Wrap[3];
+            for (int i = 0; i < wraps.Length; i++)
+            {
+                wraps[i] = w;
+            }
 
-            var services = new ServiceCollection();
+            var values = wraps.Select(x => x.Value);
+            var results = Square(values);
+            int sum = 0;
+            int count = 0;
+            foreach (var r in results)
+            {
+                count++;
+                sum += r;
+            }
+            Console.WriteLine("Count {0}", count);
+            Console.WriteLine("Sum {0}", sum);
 
-            services.AddScoped<ILogService, WhiteConsoleLogService>();
-            services.AddScoped<ILogService, GreenConsoleLogService>();
-            services.AddScoped<ILogService, RedConsoleLogService>();
-            services.AddScoped<Logger>();
-
-            using var provider = services.BuildServiceProvider();
-
-            var logService = provider.GetService<Logger>();
-
-            var eeeeeee = provider.GetServices(typeof(WhiteConsoleLogService)).FirstOrDefault() as WhiteConsoleLogService;
-
-            eeeeeee.Write("aaaaaaaaaaaaaaaaaaaa");
-
-            logService?.Log("Hello world");
-
+            Console.WriteLine("Count {0}", results.Count());
+            Console.WriteLine("Sum {0}", results.Sum());
         }
 
     }
